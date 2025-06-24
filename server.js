85c7c3ab29e01fbf6bbe85761ecbc406740782f2
Thyrail <atheosc18@gmail.com>
2025-06-24 02:28:40 +0200

    testChange

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () =>
 {
-    console.log(`Server läuft auf http://localhost:${PORT}`);
+    console.log(`Server is running on: http://localhost:${PORT}`);
 });
